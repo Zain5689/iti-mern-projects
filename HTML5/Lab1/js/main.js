dd6dedbfed3586f79fa560413fa5234142ce0d99
zainab <hlalzynbhlal@gmail.com>
2026-02-05 14:50:38 +0200

    Add CV and registration pages with corresponding styles
    - Created cv.html for displaying Zainab Hilal's CV with sections for contact, skills, achievements, and media.
    - Added me.jpg image for the CV.
    - Developed registration.html for user registration with fields for username, email, phone, experience, and password confirmation.
    - Implemented cv.css for styling the CV page, including layout, colors, and typography.
    - Created regisre.css for styling the registration form with responsive design and focus effects.

--- HTML5/Lab1/js/main.js
+++ HTML5/Lab1/js/main.js
@@ -1,4 +1,13 @@
-btn.onclick(){
-console.log( "yes" );
-}
+var ringtone = document.getElementById("ringtone");
+var contact = document.getElementsByClassName("contact")[0];
 
+contact.addEventListener("mouseenter", function () {
+  ringtone.play();
+  ringtone.controls = true;
+});
+
+contact.addEventListener("mouseleave", function () {
+  ringtone.pause();
+  ringtone.currentTime = 0;
+  ringtone.controls = false;
+});
