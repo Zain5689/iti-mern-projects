d7d59de18447f511d6c9e9767b42082e2b16c69f
zainab <hlalzynbhlal@gmail.com>
2026-02-06 14:34:01 +0200

    Add contact-chip in index

--- CSS3/Lab1/js/index.js
+++ CSS3/Lab1/js/index.js
@@ -1,13 +1,26 @@
-var ringtone = document.getElementById("ringtone");
-var contact = document.getElementsByClassName("contact")[0];
+// var ringtone = document.getElementById("ringtone");
+// var contact = document.getElementsByClassName("contact")[0];
 
-contact.addEventListener("mouseenter", function () {
-  ringtone.play();
-  ringtone.controls = true;
+// contact.addEventListener("mouseenter", function () {
+//   ringtone.play();
+//   ringtone.controls = true;
+// });
+
+// contact.addEventListener("mouseleave", function () {
+//   ringtone.pause();
+//   ringtone.currentTime = 0;
+//   ringtone.controls = false;
+// });
+
+// contact-chip
+var closeBtn = document.getElementsByClassName("close-btn")[0];
+var content = document.getElementById("info");
+var avatar = document.getElementById("avatar");
+
+closeBtn.addEventListener("click", (e) => {
+  content.classList.add("collapsed");
 });
 
-contact.addEventListener("mouseleave", function () {
-  ringtone.pause();
-  ringtone.currentTime = 0;
-  ringtone.controls = false;
+avatar.addEventListener("click", () => {
+  content.classList.toggle("collapsed");
 });
