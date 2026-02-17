9356357ad882e38af1b61c14d39ef1f20e1e57dc
zainab <hlalzynbhlal@gmail.com>
2026-02-17 15:16:30 +0200

    feat(jQuery): implement dynamic CRUD operations and table filtering
    - Added functionality to insert new rows into #persons-list based on user input.
    
    - Integrated event delegation to handle row deletion for dynamically created elements.
    
    - Developed a search feature to filter table records with case-insensitive matching.
    
    - Included UI cleanup logic to clear input fields after successful addition and search.

--- jQuery/Lab Assignments/js/crud.js
+++ jQuery/Lab Assignments/js/crud.js
@@ -1,3 +1,37 @@
 $("#add").on("click", function () {
-  console.log("add");
+  var nameValue = $("#name").val();
+  var ageValue = $("#age").val();
+  if (nameValue && ageValue) {
+    const data = `
+        <tr>
+          <td>${nameValue}</td>
+          <td>${ageValue}</td>
+          <td><button class="deleteBtn">Delete</button></td>
+        </tr>
+  `;
+    $("#persons-list tbody").append(data);
+    $("#name").val("");
+    $("#age").val("");
+  } else {
+    alert("Please enter both Name and Age!");
+  }
+});
+
+$("#persons-list").on("click", ".deleteBtn", function () {
+  $(this).closest("tr").remove();
+});
+
+$("#search").click(function () {
+  var searchItem = $("#search-item").val().toLowerCase();
+
+  $("#persons-list tbody tr").each(function () {
+    var rowText = $(this).text().toLowerCase();
+
+    if (rowText.indexOf(searchItem) > -1) {
+      $(this).show();
+    } else {
+      $(this).hide();
+    }
+  });
+  $("#search-item").val("");
 });
