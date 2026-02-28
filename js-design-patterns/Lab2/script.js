e79a0d1d0b9c4e34c92885e725c6d024c30a55e9
zainab <hlalzynbhlal@gmail.com>
2026-02-28 17:50:44 +0200

    feat: implement Decorator pattern for pizza toppings and cost calculation

--- js-design-patterns/Lab2/script.js
+++ js-design-patterns/Lab2/script.js
@@ -42,46 +42,46 @@ store.join(user2);
 store.addProduct("phone");
 
 // 2
-// class Pizza {
-//   getDescription() {
-//     return "pizza";
-//   }
-//   getCost() {
-//     return 0;
-//   }
-// }
+class Pizza {
+  getDescription() {
+    return "pizza";
+  }
+  getCost() {
+    return 0;
+  }
+}
 
-// class Margherita extends Pizza {
-//   getDescription() {
-//     return "Margherita";
-//   }
-//   getCost() {
-//     return 100;
-//   }
-// }
+class Margherita extends Pizza {
+  getDescription() {
+    return "Margherita";
+  }
+  getCost() {
+    return 100;
+  }
+}
 
-// class ToppingDecorator extends Pizza {
-//   constructor(pizza) {
-//     super();
-//     this.pizza = pizza;
-//   }
-// }
+class ToppingDecorator extends Pizza {
+  constructor(pizza) {
+    super();
+    this.pizza = pizza;
+  }
+}
 
-// class FreshTomato extends ToppingDecorator {
-//   getDescription() {
-//     return this.pizza.getDescription() + "fresh Tomato";
-//   }
+class FreshTomato extends ToppingDecorator {
+  getDescription() {
+    return this.pizza.getDescription() + "fresh Tomato";
+  }
 
-//   getCost() {
-//     return this.pizza.getCost() + 20;
-//   }
-// }
+  getCost() {
+    return this.pizza.getCost() + 20;
+  }
+}
 
-// let pizza = new Margherita();
-// pizza = new FreshTomato(pizza);
+let pizza = new Margherita();
+pizza = new FreshTomato(pizza);
 
-// console.log(pizza.getDescription());
-// console.log(pizza.getCost());
+console.log(pizza.getDescription());
+console.log(pizza.getCost());
 
 // // 3
 // class DB {
