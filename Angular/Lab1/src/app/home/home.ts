16f9b32e6c23f5495e68adb52e10592e0e1e3e38
zainab <hlalzynbhlal@gmail.com>
2026-04-12 12:37:34 +0200

    Define IUser interface for strict typing and clean code.
    Fix image pathing and source binding issues.
    
    Enhance card UI with a stats section, custom gradients, and backdrop-blur.
    
    Implement role-based alert logic and responsive layout.

--- Angular/Lab1/src/app/home/home.ts
+++ Angular/Lab1/src/app/home/home.ts
@@ -2,10 +2,33 @@ import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
 import { Footer } from '../footer/footer';
 
+interface IUser {
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+  image: string;
+  isAdmin: boolean;
+}
+
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [Navbar, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  user: IUser = {
+    id: 1,
+    name: 'Zainab Hilal',
+    email: 'zainabhilal@example.com',
+    age: 23,
+    image: 'assets/imgs/user.png',
+    isAdmin: false,
+  };
+
+  showAlert(isAdmin: boolean) {
+    alert(isAdmin ? ' Admin Dashboard' : '👤  User Profile');
+  }
+}
