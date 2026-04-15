6d48b11729493568701879018f58d856f8dded29
zainab <hlalzynbhlal@gmail.com>
2026-04-15 10:50:44 +0200

    refactor(router): define dynamic titles for movie and static pages

--- Angular/movie-app/src/app/app.routes.ts
+++ Angular/movie-app/src/app/app.routes.ts
@@ -8,9 +8,9 @@ import { MovieDetails } from './features/movies/movie-details/movie-details';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'movies', component: MovieList },
-  { path: 'about', component: About },
-  { path: 'contact', component: Contact },
-  { path: 'movies/:id', component: MovieDetails },
-  { path: '**', component: NotFound },
+  { path: 'movies', component: MovieList, title: 'movies' },
+  { path: 'about', component: About, title: 'AboutPage' },
+  { path: 'contact', component: Contact, title: 'ContactPage' },
+  { path: 'movies/:id', component: MovieDetails, title: 'MovieDetails' },
+  { path: '**', component: NotFound, title: 'NotFound' },
 ];
