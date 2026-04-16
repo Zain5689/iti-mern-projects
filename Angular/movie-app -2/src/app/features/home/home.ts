7aa3134d358b1bc138e9aa33bb1bfeeaf5a41596
zainab <hlalzynbhlal@gmail.com>
2026-04-16 13:55:24 +0200

    feat: implement home page with popular movies grid and testimonials section

--- Angular/movie-app -2/src/app/features/home/home.ts
+++ Angular/movie-app -2/src/app/features/home/home.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { MovieService } from './../movies/services/movie';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { TestimonialCard } from '../../shared/components/testimonial-card/testimonial-card';
+import { imgPrefix } from '../../shared/models/data/movies.data';
 
 @Component({
   selector: 'app-home',
-  imports: [TestimonialCard],
+  standalone: true,
+  imports: [TestimonialCard, CommonModule, RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  private movieService = inject(MovieService);
+  private router = inject(Router);
+
+  popularMovies = this.movieService.getAllMovies().slice(0, 4);
+  readonly imgPrefix = imgPrefix;
+
+  goToDetails(id: number) {
+    this.router.navigate(['/movies', id]);
+  }
+}
