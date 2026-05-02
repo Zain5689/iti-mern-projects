f982c7c1953d4f6a9b5bc5bec25ce0a200a11c12
zainab <hlalzynbhlal@gmail.com>
2026-05-02 13:51:12 +0300

    feat: add endpoint to retrieve all todos

--- Docker/node-todo-app/server.js
+++ Docker/node-todo-app/server.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to my Node.js Todo App!");
 });
 
+app.get("/todos", (req, res) => {
+  res.send("Here you will see all your todos.");
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
